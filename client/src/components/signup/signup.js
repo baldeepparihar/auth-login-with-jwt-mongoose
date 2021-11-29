af4a4f174ec25a423a4af4a237e2ae69841304ad
baldeepparihar <75191811+baldeepparihar@users.noreply.github.com>
2021-11-29 13:04:19 -0800

    styled signup form

--- client/src/components/signup/signup.js
+++ client/src/components/signup/signup.js
@@ -4,21 +4,23 @@ import './signup.css';
 function Signup() {
     return(
         <div className="signup">
-                <h1>Please fill out the sign up form.</h1>
+                <h1>Please fill out the form.</h1>
             <form className="signup__form" action="submit">
-                <div>
+                <div className="signup__form--names">
                     <label htmlFor="firstName">First Name</label>
                     <input id="firstName" type="text" />
                     <label htmlFor="lastName">Last Name</label>
                     <input id="lastName" type="text" />
                 </div>
+                <div className="signup__form--email" >
                     <label htmlFor="email">Email</label>
-                    <input type="email" />
-                <div>
+                    <input type="email" id="email" />
+                </div>
+                <div className="signup__form--passwords">
                     <label htmlFor="password">Password</label>
-                    <input type="password" />
-                    <label htmlFor="repeat-password"></label>
-                    <input type="repeat-password" />
+                    <input type="password" id="password" />
+                    <label htmlFor="re-type-password">Re-Type Password</label>
+                    <input type="password" id="password" />
                 </div>
             </form>
         </div>
