5595294f3a0ef7a84c3c3715e9dba56fa146fd40
baldeepparihar <75191811+baldeepparihar@users.noreply.github.com>
2021-11-28 21:37:25 -0800

    both server and client

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect('mongodb://localhost:27017');
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.listen(PORT, () => {
+    console.log('Listening on ' + PORT);
+})
