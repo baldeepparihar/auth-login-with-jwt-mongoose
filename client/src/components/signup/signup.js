e05558671e7cbc45a0f079e65ddda540ae7e6ba5
baldeepparihar <75191811+baldeepparihar@users.noreply.github.com>
2021-11-29 23:08:42 -0800

    moving labels in input focus

--- client/src/components/signup/signup.js
+++ client/src/components/signup/signup.js
@@ -9,20 +9,20 @@ function Signup() {
             </div>
             <form className="signup__form" action="submit">
                 <div className="signup__form--names">
-                    <label className="firstName-label" htmlFor="firstName">First Name</label>
                     <input id="firstName" class="firstName-input" type="text" />
-                    <label className="lastName-label" htmlFor="lastName">Last Name</label>
+                    <label className="firstName-label" htmlFor="firstName">First Name</label>
                     <input id="lastName" className="lastName-input" type="text" />
+                    <label className="lastName-label" htmlFor="lastName">Last Name</label>
                 </div>
                 <div className="signup__form--email" >
-                    <label className="email-label" htmlFor="email">Email</label>
                     <input className="email-input" type="email" id="email" />
+                    <label className="email-label" htmlFor="email">Email</label>
                 </div>
                 <div className="signup__form--passwords">
-                    <label className="password-label" htmlFor="password">Password</label>
                     <input className="password-input" type="password" id="password" />
-                    <label className="retypePassword-label" htmlFor="re-type-password">Re-Type Password</label>
+                    <label className="password-label" htmlFor="password">Password</label>
                     <input className="retypePassword-input" type="password" id="password" />
+                    <label className="retypePassword-label" htmlFor="re-type-password">Re-Type Password</label>
                 </div>
             </form>
         </div>
