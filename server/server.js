265ac342a5a312d76260542de3a039bca8f7083e
baldeepparihar <75191811+baldeepparihar@users.noreply.github.com>
2021-11-30 19:38:30 -0800

    User mongoose Schema set up

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const User = require('./models/user.model');
 const jwt = require('jsonwebtoken');
 
 app.use(cors());
